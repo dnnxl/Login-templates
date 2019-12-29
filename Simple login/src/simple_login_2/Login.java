12407550bafa53f3a86915f691a98f767bedbfeb
Danny Xie Li <51223285+dnnxl@users.noreply.github.com>
2019-12-28 20:40:43 -0600

    Create Login.java

---
+++ Simple login/src/simple_login_2/Login.java
@@ -0,0 +1,38 @@
+package simple_login_2;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Login implements Initializable{
+
+    @FXML private TextField password_textfield;
+	  @FXML private TextField user_textfield;
+    @FXML private Button signIn_button;
+    @FXML private Button reset_button;
+
+    public void signIn()
+	{
+        String user = this.user_textfield.getText().toString();
+        String password = this.password_textfield.getText().toString();
+        System.out.println("User " + user);
+        System.out.println("Password " + password);
+    }
+
+    public void reset()
+	{
+        String user = this.user_textfield.getText().toString();
+        String password = this.password_textfield.getText().toString();
+        System.out.println("User " + user);
+        System.out.println("Password " + password);
+    }
+    
+    @Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+	}
+}
